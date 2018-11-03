45ddf6b8bf5fe395a9840a7acb996bc49878109b
tery15002 <44665083+tery15002@users.noreply.github.com>
2018-11-03 13:00:49 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,34 +1,38 @@
 ﻿<?php
     $accessToken = "8uYL7S31MNpY8u5rl32AJrIJ8HKIIPsPFBHbcYgCklGSuzDRsK1pu2jT+scopLs0co8yiTpBrYg4uWruJCFkFg6M1metrmSKvi4ZRlSGc5bdKul2/CDCwuIjIzpSoDpvpyd7NNYFqgzhriwl75ZV4gdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
-    
+
     $content = file_get_contents('php://input');
     $arrayJson = json_decode($content, true);
-    
+
     $arrayHeader = array();
     $arrayHeader[] = "Content-Type: application/json";
     $arrayHeader[] = "Authorization: Bearer {$accessToken}";
-    
+
+    ######################  Set Text Variable ###########################
+    $TXT_DIRECTION = "นำทาง";
+    $TXT_FINDPLACE = "จะไป";
+
+    ######################  Set Text ###########################
     //รับข้อความจากผู้ใช้
     $message = $arrayJson['events'][0]['message']['text'];
-	// $message = "จำนำไป";
-
+//    $message = "ไป";
+    ######################  Set Conditions ###########################
 	$pos_por = strpos($message,"ป้ออ");
 
-	$pos_where = strpos($message,"นำทาง");
+    $pos_where = strpos($message,"จะไป");
 
 	if($pos_por !== false){
 	    $message = 'tik';
 	}
 
-	$txt 	= null;
-	$src = null;
-	$origin = "ตลาดพลู";
-	$destination = "วุทากาศ";
-	$__Direction = null; 
+	$txt  = null;
+	$src  = null;
+	$__Direction = null;
 
-	if( strpos($message,"นำทาง") !== false)
+
+if( strpos($message,$TXT_DIRECTION) !== false )
 	{
-		$src = explode_text("นำทาง",$message);
+		$src = explode_text($TXT_DIRECTION,$message);
 		if($src[0] != 'where'){
 			$__DIRECTION = 'https://www.google.com/maps/dir/?api=1&origin='.$src[0].'&destination='.$src[1].'&travelmode=driving';
 			$message = "direct";
@@ -37,14 +41,13 @@
 		}
 
 	}
-	else if( strpos($message,"ไป") !== false )
+	else if( strpos($message,$TXT_FINDPLACE) !== false )
 	{
-		$src =  explode_text("ไป",$message);
+		$src =  explode_text($TXT_FINDPLACE,$message);
 		$__DIRECTION = 'https://www.google.com/maps/search/?api=1&query='.$src[1];
 		$message = $src[0];
 	}
- 
-
+    ######################  Set txt respond  ###########################
 		$res_txt_por =  [
 							'tik' =>[
 			                        'เรียกไม',
@@ -59,8 +62,8 @@
 			                        'จ๋าา'
 			                        ],
 			                'findWay' => $__DIRECTION,
-			                'direct' => $__DIRECTION,
-			                'where'	 =>'จะไปไหนดีล่ะ -- อยากไปไหนพิมพ์ ไป ตามด้วยสถานที่ เช่น "ไปจุตจักร" -- หรือนำทางพิมพ์ "นำทางจากเอกชัย8ไปสยาม"'
+			                'direct'  => $__DIRECTION,
+			                'where'	  =>'จะไปไหนดีล่ะ -- อยากไปไหนพิมพ์ จะไป ตามด้วยสถานที่ เช่น "จะไปจุตจักร" -- หรือนำทางพิมพ์ "นำทางจากเอกชัย8ไปสยาม"'
 	               		];
 
 
@@ -97,7 +100,7 @@ function replyMsg($arrayHeader,$arrayPostData){
         curl_setopt($ch, CURLOPT_URL,$strUrl);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
         curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -108,12 +111,12 @@ function replyMsg($arrayHeader,$arrayPostData){
 
 //check Text
 function explode_text($txt,$msg)
-{	
+{ $res = [];
 		if($txt == "นำทาง")
-		{	
+		{
 			$ori  = null;
 			$dest = null;
-			$explode_ 	= explode("นำทาง", $msg); //นำทางจากเอกชัย8ไปสยาม
+			$explode_ 	= explode($txt, $msg); //นำทางจากเอกชัย8ไปสยาม
 
 			 if($explode_[1]===""){
 			    $ori = 'where';
@@ -123,21 +126,19 @@ function explode_text($txt,$msg)
 				$ori 		= explode("จาก",$textSearch[0]); //จากเอกชัย8 => [0]=>จาก,[1]=>เอกชัย8
 				$dest	= $textSearch[1]; //สยาม
 			 }
+			$res = [$ori[1],$dest];
 
-
-			return [$ori[1],$dest];
-
-		}else if($txt == "ไป"){
-		    $explode_ = explode('ไป',$msg);
+		}else if($txt == "จะไป"){
+		    $explode_ = explode($txt,$msg);
 		    if($explode_[1]===""){
 			    $m = 'where';
 		    }else{
 			    $t = trim($explode_[1]);
 			    $m = 'findWay';
-		    }	
-		    return [$m,$t];
+		    }
+		    $res = [$m,$t];
 		}
-
+    return $res;
 }
 
 
