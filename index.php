bea2c07b377dd19bff9d2547e0e88d90f2215c68
tery15002 <44665083+tery15002@users.noreply.github.com>
2018-11-03 21:18:01 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -79,15 +79,15 @@ function getResponse($message,$arrayJson,$arrayHeader,$res_txt_por,$userid)
     $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
     $arrayPostData['messages'][0]['type'] = "text";
     if($message == 'tik'){
-        $arrayPostData['messages'][0]['text'] = $userid."=>".$res_txt_por['tik'][array_rand($res_txt_por['tik'],1)];
+        $arrayPostData['messages'][0]['text'] = $res_txt_por['tik'][array_rand($res_txt_por['tik'],1)];
     }else if($message == 'google'){
-        $arrayPostData['messages'][0]['text'] = $userid."=>".$res_txt_por['google'];
+        $arrayPostData['messages'][0]['text'] = $res_txt_por['google'];
     }else if($message == 'findWay'){
-        $arrayPostData['messages'][0]['text'] = $userid."=>".$res_txt_por['findWay'];
+        $arrayPostData['messages'][0]['text'] = $res_txt_por['findWay'];
     }else if($message == 'direct'){
-        $arrayPostData['messages'][0]['text'] = $userid."=>".$res_txt_por['direct'];
+        $arrayPostData['messages'][0]['text'] = $res_txt_por['direct'];
     }else if($message == 'where'){
-        $arrayPostData['messages'][0]['text'] = $userid."=>".$res_txt_por['where'];
+        $arrayPostData['messages'][0]['text'] = $res_txt_por['where'];
     }
 	print_r($arrayPostData);
 
@@ -111,7 +111,7 @@ function replyMsg($arrayHeader,$arrayPostData){
         curl_close ($ch);
     }
    exit;
-
+######## Function #########
 //check Text
 function explode_text($txt,$msg)
 { $res = [];
